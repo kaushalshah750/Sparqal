8fc90a8f7c920003a30e7d581124666224d90cb5
kaushalshah750 <kaushalshah750@gmail.com>
2025-06-07 20:18:06 +0530

    removed static login cred

--- Frontend/src/pages/Admin/Login.jsx
+++ Frontend/src/pages/Admin/Login.jsx
@@ -19,8 +19,8 @@ import { toast } from "sonner";
 
 const SparqalLogin = () => {
   const [showPassword, setShowPassword] = useState(false);
-  const [email, setEmail] = useState("kaushal@example.com");
-  const [password, setPassword] = useState("Kaushal$#@#123");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
